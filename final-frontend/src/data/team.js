8282a7d47d7e314bb9a989a2662c63f9882dbf2a
imak02 <asbinkhanal222@gmail.com>
2023-07-08 09:12:55 +0545

    Data for about page

---
+++ final-frontend/src/data/team.js
@@ -0,0 +1,32 @@
+const team = [
+  {
+    id: 1,
+    name: "Asbin Khanal",
+    role: "Frontend Developer",
+    image: "/asbin.jpg",
+  },
+  {
+    id: 2,
+    name: "Manita Kunwor",
+    role: "Backend Developer",
+    image: "/manita.jpg",
+  },
+  {
+    id: 3,
+    name: "Aayush Regmi",
+    role: "System Analyst",
+    image: "/aayush.jpg",
+  },
+  {
+    id: 4,
+    name: "Thomas Basyal",
+    role: "Project Manager",
+    image: "/thomas.jpg",
+  },
+  {
+    id: 5,
+    name: "Rohil Maharjan",
+    role: "Designer",
+    image: "/rohil.jpg",
+  },
+];
--- final-frontend/src/pages/About.jsx
+++ final-frontend/src/pages/About.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Carousel from "../components/Carousel";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { Info } from "@mui/icons-material";
+import Banner from "../components/Banner";
+import TeamMember from "../components/TeamMember";
 
 const About = () => {
   return (
     <Box sx={{ minHeight: "100vh" }}>
-      <Carousel />
+      <Banner
+        title="Characteristics of a successful team"
+        description="Successful teams often exhibit strong communication and collaboration skills, a shared sense of purpose and goals, mutual trust and respect among team members, effective leadership, and a diverse range of skills and expertise that complement each other. "
+        button="Learn More"
+        btnIcon={<Info />}
+        bgImage="https://images.unsplash.com/photo-1510146758428-e5e4b17b8b6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        focusText="VFC Blogs"
+      />
+
+      <Box>
+        <Typography
+          variant="h4"
+          component="h5"
+          sx={{ fontWeight: "bold", textAlign: "center" }}
+        >
+          Our Team
+        </Typography>
+
+        <Box>
+          <TeamMember />
+        </Box>
+      </Box>
     </Box>
   );
 };
