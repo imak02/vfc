4b6837146da86c99a14d35317ea9cf2f06b6bae9
imak02 <asbinkhanal222@gmail.com>
2024-05-09 15:47:14 +0545

    Change backend endpoint to render api

--- final-frontend/src/api/axiosDefaults.js
+++ final-frontend/src/api/axiosDefaults.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 // axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API;
-axios.defaults.baseURL = "http://localhost:8000";
+// axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://vfc-backend-0q0l.onrender.com";
 console.log(Cookies.get("token"));
 axios.defaults.headers.common["Authorization"] = `Token ${Cookies.get(
   "token"
